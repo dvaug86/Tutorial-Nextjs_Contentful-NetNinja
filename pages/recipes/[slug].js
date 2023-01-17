5388f1c3790c467e55dc9c54081a292826ca7f14
dv <dvaug86@gmail.com>
2023-01-17 11:29:20 -0500

    redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -29,6 +30,16 @@ export async function getStaticProps({ params }) {
     content_type: "recipe",
     "fields.slug": params.slug,
   });
+
+if(!items.length){
+  return {
+    redirect: {
+      destination: '/',
+      permanent: false
+    }
+  }
+}
+
   return {
     props: { recipe: items[0] },
     revalidate: 1
@@ -36,7 +47,7 @@ export async function getStaticProps({ params }) {
 }
 
 export default function RecipeDetails({ recipe }) {
-  if(!recipe) return  <div>loading</div>
+  if(!recipe) return  <Skeleton />
   
   const { featuredImage, title, cookingTime, ingredients, method } =recipe.fields;
     
